06480e67785383118c1614f560207c0fdc408f6c
Jorge Rivera <jorgeriv@protonmail.com>
2023-09-05 18:22:07 -0500

    :rocket: Create a new header component

---
+++ src/components/header-bar.vue
@@ -0,0 +1,18 @@
+<template>
+	<!-- As a heading -->
+	<nav class="navbar bg-body-tertiary" id="mainHeader">
+		<div class="container-fluid">
+			<span class="navbar-brand mb-0 h1">Navbar</span>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'HeaderBar',
+}
+</script>
+
+<style>
+
+</style>
