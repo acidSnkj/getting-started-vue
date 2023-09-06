2382b8bcdf3be82c1551c22d6a19b0977b045172
Jorge Rivera <jorgeriv@protonmail.com>
2023-09-06 17:47:22 -0500

    :wrench: WIP Car List

---
+++ src/components/Car/List/CarList.vue
@@ -0,0 +1,61 @@
+<template>
+	<div v-for="car in cars" :key="car.name">
+		<div class="card" style="width: 18rem;">
+			<div class="card-body">
+				<h5 class="card-title">{{ car.name }}</h5>
+				<p class="card-text">{{ car.description }} </p>
+				<button type="button" class="btn btn-primary" @click="selectedCar = car">Seleccionar</button>
+			</div>
+		</div>
+	</div>
+	<div v-if=selectedCar>
+		<span>Selected car</span>
+		<span>{{ selectedCar.name }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CarList',
+	data() {
+		return {
+			selectedCar: undefined,
+			cars: [
+				{
+					name: "Mazda 6",
+					description: "Beautiful car",
+					engine: '',
+					transmission: '',
+					year: 'YYYY-MM-DD',
+					cc: 5000,
+					active: false,
+					color: 'Black',
+					tr: 'FR',
+				},
+				{
+					name: "Ford Fiesta",
+					description: "Meh Car",
+					engine: '1.6L',
+					transmission: 'Boring Automatic',
+					year: 'YYYY-MM-DD',
+					cc: 1600,
+					active: false,
+					color: 'Grey',
+					tr: 'FF',
+				},
+				{
+					name: "Chevrolet Aveo",
+					description: "Not Okay Car",
+					engine: '1.2L',
+					transmission: 'Boring in every TR',
+					year: 'YYYY-MM-DD',
+					cc: 1200,
+					active: false,
+					color: 'Ugly',
+					tr: 'FF',
+				},
+			]
+		}
+	}
+}
+</script>
--- src/components/Car/SpecTech/SpecTech.vue
+++ src/components/Car/SpecTech/SpecTech.vue
@@ -1,11 +1,55 @@
 <template>
 	<!-- TODO: Crear vista detalle del carro seleccionado -->
-	<span>Vista detalle carro</span>
+	<div class="container">
+		<span>Vista detalle carro</span>
+		<span>{{ car.name }}</span>
+		<input type="text" v-model=car.name />
+		<button class="btn btn-secondary" @click=cancel>Cancel</button>
+		<button class="btn btn-primary" @click=save>Save</button>
+		<div class="form-check">
+			<input class="form-check-input" type="checkbox" v-model=car.active id="flexCheckDefault">
+			<label class="form-check-label" for="flexCheckDefault">
+				Active
+			</label>
+		</div>
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" v-model=car.color
+				value="green">
+			<label class="form-check-label" for="flexRadioDefault1">
+				Green
+			</label>
+		</div>
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" v-model=car.color
+				value="red">
+			<label class="form-check-label" for="flexRadioDefault2">
+				Rojo diamante
+			</label>
+			<div class="selector-dad">
+				<div :class="{ selectorChild: !car.tr }">
+				</div>
+				<select class=" form-select" v-model="car.tr" aria-label="Default select example" @keyup.esc="clear">
+					<option disabled value>Selecccione tipo de traccion
+					</option>
+					<option>Traccion Delantera</option>
+					<option>Traccion Trasera</option>
+					<option>Traccion en las 4 ruedas</option>
+				</select>
+			</div>
+
+		</div>
+		<div :style="{ 'background-color': car.color, 'width': '40px', 'height': '20px' }" />
+		<div class="badge text-bg-success">
+			<pre>
+			{{ message }}
+		</pre>
+		</div>
+	</div>
 </template>
 
 
 <script>
- export default {
+export default {
 	name: 'SpecTech',
 	data() {
 		/* TODO: Determinar atributos del carro
@@ -26,8 +70,42 @@
 				transmission: '',
 				year: 'YYYY-MM-DD',
 				cc: 5000,
-			}
+				active: false,
+				color: '',
+				tr: '',
+			},
+			message: ''
 		}
 	},
- }
+	methods: {
+		cancel() {
+			this.message = '';
+		},
+		save() {
+			this.message = JSON.stringify(this.car, null, '\n');
+		},
+		clear() {
+			this.car.tr = '';
+		}
+	},
+}
 </script>
+<style>
+.container {
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+}
+
+.selector-dad {
+	display: flex;
+	justify-content: space-between;
+}
+
+.selectorChild {
+	width: 10px;
+	height: 40px;
+	background-color: red;
+	border-radius: 10px;
+}
+</style>
